e13885ca5296f920c547a3f7d138422eab3c2108
geebeechu <pannipa.da@ku.th>
2021-02-23 16:32:50 +0700

    IF_ELSE

---
+++ if_else/if_else1.py
@@ -0,0 +1,21 @@
+# EX 1 
+score = int(input("ENTER YOUR SCORE :::: "))
+
+if score <= 49 :
+    print("GRADE = F")
+elif score < 55 :
+    print("GRADE = D")
+elif score < 60 :
+    print("GRADE = D+")
+elif score < 65 :
+    print("GRADE = C")
+elif score < 70 :
+    print("GRADE = C+")
+elif score < 75 :
+    print("GRADE = B")
+elif score < 80 :
+    print("GRADE = B+")
+else score >= 80 :
+    print("GRADE = A")
+
+
