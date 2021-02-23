5de6525b4aac69defe9aaeca783ff1a9b821ec5e
geebeechu <pannipa.da@ku.th>
2021-02-23 16:10:15 +0700

    Srting

---
+++ String/f-string.py
@@ -0,0 +1,6 @@
+name = "Ja"
+age = 23
+
+result = f" My name is {name}, and I am {age}"
+
+print("result", result)  # Output: "My name is Ja, and I am 23"
---
+++ String/format.py
@@ -0,0 +1,6 @@
+age = 23
+txt = "my name is Ja, and I am {}"
+
+result = txt.format(age)
+
+print("result", result)  # Output: "My name is Jim, and I am 23"
